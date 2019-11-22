95320117df7ee26ebd27ea554667d5e6a2c6d6bc
trainee1 <erickakegil@outlook.es>
2019-11-22 13:37:25 -0600

    search v2

--- search.js
+++ search.js
@@ -1,17 +1,16 @@
 const search = (findSearch) => {
-const animals = ["Caballo", "Oso", "Perro"];
-const persons = ["Pedro", "Maria", "Rosa"];
-const stuffs = ["Petroleo", "Carne", "Rosca"];  
-const wordsFind = [];
+  const animals = ["Caballo", "Oso", "Perro"];
+  const persons = ["Pedro", "Maria", "Rosa"];
+  const stuffs = ["Petroleo", "Carne", "Rosca"];  
+  const wordsFound = [];
   
-words = animals.concat(persons, stuffs);
-words.forEach(function (word) {
-  if(findSearch.toLowerCase()  == word.toLowerCase() .substr(0,findSearch.length)){
-    wordsFind.push(word);
-  }
-});
-  
-return wordsFind;
+  words = animals.concat(persons, stuffs);
+  words.forEach(function (word) {
+    if(findSearch.toLowerCase() == word.toLowerCase().substr(0, findSearch.length)){
+      wordsFound.push(word);
+    }
+  }); 
+  return wordsFound;
 }  
   
 search("Ca");
