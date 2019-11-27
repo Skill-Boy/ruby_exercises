b571e11b0d83d8711c168c755e13ec21f7e02944
trainee1 <erickakegil@outlook.es>
2019-11-27 16:02:08 -0600

    diagonals

--- diagonals.rb
+++ diagonals.rb
@@ -7,8 +7,7 @@ def diagonals_sum(numbers)
     horizontalSum << numbers[key].sum
   }
     
-  verticalSum.max
-  horizontalSum.max
+  verticalSum.max > horizontalSum.max ? verticalSum.max : horizontalSum.max  
 end
   
 diagonals_sum([[6, 7, 8], [1, 4, 29], [4, 78, 12]])
