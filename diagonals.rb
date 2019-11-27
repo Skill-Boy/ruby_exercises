e0d3a94aeb933939deea3b3228370597cb221c8f
trainee1 <erickakegil@outlook.es>
2019-11-27 16:08:22 -0600

    diagonals

--- diagonals.rb
+++ diagonals.rb
@@ -1,13 +1,13 @@
 def diagonals_sum(numbers)
-  verticalSum = []
-  horizontalSum = []
+  vertical_sum = []
+  horizontal_sum = []
     
   numbers.transpose.each_with_index { |number, key|
-    verticalSum << number.sum
-    horizontalSum << numbers[key].sum
+    vertical_sum << number.sum
+    horizontal_sum << numbers[key].sum
   }
     
-  verticalSum.max > horizontalSum.max ? verticalSum.max : horizontalSum.max  
+  vertical_sum.max > horizontal_sum.max ? vertical_sum.max : horizontal_sum.max  
 end
   
 diagonals_sum([[6, 7, 8], [1, 4, 29], [4, 78, 12]])
