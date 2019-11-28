0bab728013b4de1215bd4d91cb80bbba7d571a5a
Erick <erickakegil@outlook.es>
2019-11-27 21:54:06 -0600

    diagonals

--- diagonals.js
+++ diagonals.js
@@ -2,17 +2,17 @@ const diagonalSum = numbers => {
   horizontalSum = [];
   verticalSum = numbers[0].map((column, iteration) => numbers.map(row => row[iteration]));
   verticalMax = [];
-  
-  numbers.forEach(function(number, key) {
-    horizontalSum.push(number.reduce((numberOne, numberTwo) => numberOne + numberTwo, 0));
-  });
-  
-  verticalSum.forEach(function(number, key){
-    verticalMax.push(number.reduce((numberOne, numberTwo) => numberOne + numberTwo, 0));
-  });
-    
-  if (Math.max(...verticalMax) > Math.max(...horizontalSum)) return Math.max(...verticalMax)
-    return Math.max(...horizontalSum)
+
+  if (diagonals(verticalSum, verticalMax) > diagonals(numbers, horizontalSum)) return diagonals(verticalSum, verticalMax);
+    return diagonals(numbers, horizontalSum);  
 }
+
+const diagonals = (diagonalNums, diagonal) => {
+  diagonalNums.forEach(function(number, key){
+    diagonal.push(number.reduce((numberOne, numberTwo) => numberOne + numberTwo, 0)); 
+  });
+  
+  return Math.max(...diagonal);
+} 
   
 diagonalSum([[6, 7, 8], [1, 4, 29], [4, 78, 12]]);
